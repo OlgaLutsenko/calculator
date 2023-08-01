8960dcf36da00911a766c5d5967d0b16cb92117e
Olga <olga.lutsenko.95@mail.ru>
2023-08-01 18:50:00 +0200

    C
    (cherry picked from commit 3afd1f13db1c1746e47f876d957aaa3835bcb3ba)

--- src/Calculator/Calculator.java
+++ src/Calculator/Calculator.java
@@ -10,8 +10,10 @@ public class Calculator {
         String[] regexActions = {"\\+", "-", "\\*", "/"};  //экранированные знаки действия. для деления выражения на две части
 
         Scanner scan = new Scanner(System.in);
+
         System.out.print("Введите арифметическое выражение: ");
         String exp = scan.nextLine();
+
 
         //определяем введённое действие
         int actionIndex = -1;
@@ -20,14 +22,16 @@ public class Calculator {
                 actionIndex = i;
                 break;
             }
+
         }
+
 
         //если введено НЕ арифметическое действие, завершить программу
         if (actionIndex == -1) {
             System.out.println("Некорректное выражение");
             return;
         }
-        //10  строка
+        //10  строка //сплит делит строчку
         String[] data = exp.split(regexActions[actionIndex]);
 
         //определяем оба ли числа римские (арабские)
@@ -40,6 +44,11 @@ public class Calculator {
                 //если римские перевести в арабские
                 a = conv.RomanToInt(data[0]);
                 b = conv.RomanToInt(data[1]);
+
+                if (a == -1 || b == -1) {
+                    System.out.println("Римское число не может быть отрицательным");
+                    return;
+                }
             } else {
                 //конвертируем арабские числа из строки в число
                 a = Integer.parseInt(data[0]);
@@ -64,6 +73,8 @@ public class Calculator {
                         System.out.println("Деление на ноль невозможно");
                         return;
                     }
+
+
                 default:
                     result = a / b;
                     break;
@@ -76,12 +87,14 @@ public class Calculator {
                 //если числа арабские, реультат в арабском числе
                 System.out.println(result);
             }
-        } else{
-                System.out.println("Числа должны быть в одном формате");
-            }
 
 
+            } else {
+            System.out.println("Числа должны быть в одном формате");
         }
+
     }
+
+}
 
 
